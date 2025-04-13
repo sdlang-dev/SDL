6b26faa7622b38126350787421de7f2b0c8e048e
Samael <singingbush@hotmail.com>
2025-04-13 23:48:55 +0100

    #62: SDLang as a PropertySource for Spring Framework (#125)
    * Add sub module for using SDLang as a Spring PropertySource
    
    * Use logback 1.3.* until JDK 11 is minimum

---
+++ spring/src/main/java/com/singingbush/sdl/spring/SdlPropertySourceFactory.java
@@ -0,0 +1,28 @@
+package com.singingbush.sdl.spring;
+
+import com.singingbush.sdl.Parser;
+import com.singingbush.sdl.SDLParseException;
+import com.singingbush.sdl.Tag;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertySourceFactory;
+import org.springframework.lang.Nullable;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class SdlPropertySourceFactory implements PropertySourceFactory {
+    @Override
+    public PropertySource<?> createPropertySource(@Nullable String name, EncodedResource resource) throws IOException {
+        try(final InputStream i = resource.getInputStream()) {
+            final List<Tag> tags = new Parser(new InputStreamReader(i, StandardCharsets.UTF_8)).parse();
+
+            return name != null ? new SDLangTagSource(name, tags) : new SDLangTagSource("sdl", tags);
+        } catch (final SDLParseException e) {
+            throw new RuntimeException("Unable to parse SDLang property source", e);
+        }
+    }
+}
